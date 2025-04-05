799cff81a2293b514fafbdd70e1fa1c6ce124790
b3nh <38316487+b3nh@users.noreply.github.com>
2025-04-05 21:34:59 +1100

    fixing lint errors

--- src/app/api/auth/[...auth0]/route.ts
+++ src/app/api/auth/[...auth0]/route.ts
@@ -1,4 +1,4 @@
-import { handleAuth, handleLogin, handleLogout } from '@auth0/nextjs-auth0';
+import { handleAuth, handleLogout } from '@auth0/nextjs-auth0';
 
 export const GET = handleAuth({
   logout: handleLogout({
